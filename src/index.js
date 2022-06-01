2763bb97c715baf93b02d1f65b092e23a15ce92b
Jacob <jacobkyalo43@gmail.com>
2022-06-01 19:40:53 +0300

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 const display = document.querySelector("#screen");
 const buttons = document.querySelectorAll(".button");
+const opr = document.querySelectorAll(".opr");
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    display.innerHTML = button.innerHTML;
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    buttonValue = button.innerText;
+    if (buttonValue == opr) {
+      display.innerText += buttonValue;
+    } else if (buttonValue == "AC") {
+      display.innerText = "";
+    } else if (buttonValue == "DEL") {
+      display.innerText = display.innerText.slice(0, -1);
+    } else if (buttonValue == "=") {
+      try {
+        display.innerText = eval(display.innerText);
+      } catch (error) {
+        display.innerText = "operation error!";
+      }
+    } else {
+      display.innerText += buttonValue;
+    }
   });
 });
